1d7d140f9bcbddb9dcc14338c0a0bed8739850ea
tomiwapaul <adebanjoakeem8@gmail.com>
2023-02-01 08:43:50 -0800

    created signup and login pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,27 @@
 
 import './App.css';
 import Signup from './Components/Signup/Signup';
-
+import {BrowserRouter,Routes,Route } from 'react-router-dom'
+import Home from './Components/Home';
+import Header from './Components/Partials/Header';
+import NotFound from "./Components/Auth/NotFound";
+import Login from './Components/Auth/Login/Login';
 function App() {
   return (
-    <div className="App">
-      <Signup />
-</div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route index element={<Home />}/>
+          <Route path="/signup"element={<Signup />}/>
+          <Route path="/login"element={<Login/>}/>
+          <Route path="*"element={<NotFound />}/>
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
---
+++ client/src/Components/Auth/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import "./NotFound.css";
+const NotFound = () => {
+  return (
+    <div>Opp! 404 Page Not Found!</div>
+  )
+}
+
+export default NotFound
