066c4acc019510e2979f705b878499538b73b1e2
tomiwapaul <adebanjoakeem8@gmail.com>
2023-02-06 04:20:05 -0800

    created edit page

--- client/src/Components/Auth/Login/Contact/EditContact.js
+++ client/src/Components/Auth/Login/Contact/EditContact.js
@@ -2,8 +2,33 @@ import React from 'react'
 import "./EditContact.css"
 const EditContact = () => {
   return (
-    <div>Edit Contact</div>
-  )
+    <div className="edit-contact-wrapper">
+      <h2 className='name-edit'>Edit Contact</h2><br/><br/>
+      <div className="edit-grp-1">
+        <input type="text" placeholder="first name" />
+      </div>
+      <br />
+
+      <div className="edit-grp-2">
+        <input type="text" placeholder="last name" />
+      </div>
+      <br />
+
+      <div className="edit-grp-3">
+        <input type="text" placeholder="phone" />
+      </div>
+      <br />
+
+      <div className="edit-grp-4">
+        <input type="email" placeholder="email" />
+      </div>
+      <br />
+
+      <div className="edit-rows">
+        <button type="submit">Update Contact</button>
+      </div>
+    </div>
+  );
 }
 
 export default EditContact
